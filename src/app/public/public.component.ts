f05e8fffc35e29920d5ea83e01fd380ea4af8721
SandeepKumar377 <sandeepkumar.sk5.ss55@mail.com>
2022-10-16 22:27:23 +0530

    routerLink

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicComponent implements OnInit {
 
+  bookId:number=12;
+  author:string='Sandeep';
+
   constructor() { }
 
   ngOnInit(): void {
