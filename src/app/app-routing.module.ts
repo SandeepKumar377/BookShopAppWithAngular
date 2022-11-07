2ca1058042eaf6428c78209e92cca51926120c42
SandeepKumar377 <sandeepkumar.sk5.ss55@mail.com>
2022-11-07 23:17:35 +0530

    redirectTo in angular route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
  
 const routes : Routes=[
+  {path:'', redirectTo:'about-us', pathMatch:'full'},
   {path:'about-us', component: AboutUsComponent},
   {path:'contact-us', component: ContactUsComponent},
   {path:'**', component: NotFoundComponent},
