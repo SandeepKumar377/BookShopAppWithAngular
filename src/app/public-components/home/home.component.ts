0cac27b728cd93160e9934d38da145ee0a0561f4
SandeepKumar377 <sandeepkumar.sk5.ss55@mail.com>
2023-01-15 18:59:11 +0530

    @ViewChild component

--- src/app/public-components/home/home.component.ts
+++ src/app/public-components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { AuthorsComponent } from 'src/app/shared/components/authors/authors.component';
 import { CounterService } from 'src/app/shared/services/counter.service';
 
 @Component({
@@ -7,10 +9,22 @@ import { CounterService } from 'src/app/shared/services/counter.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  @ViewChild('authors') private childAuthor!: AuthorsComponent;
+  @ViewChild('title') private title!: ElementRef;
+  @ViewChild('decButton') private decButton!: MatButton;
   constructor(public couterService: CounterService) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void{
+    setTimeout(()=>{
+      this.childAuthor.setData(11);
+      this.decButton.color= 'primary';
+      this.decButton.disabled=true;
+      console.log(this.title);
+      this.title.nativeElement.innerHTML= 'This is updated text!';
+    },0);
   }
 
   public increase(): void{
